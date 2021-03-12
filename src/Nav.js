b3225c4dd18b63507944afc4373b13c46516978f
Parth Ardeshana <parthardeshana@users.noreply.github.com>
2021-03-12 10:34:37 +0530

    a

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from './images/netflix-logo.png';
 import smiley from './images/smiley-face.svg';
 import './Nav.css'
 
 function Nav() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else { handleShow(false) }
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        };
+    }, []);
     return (
-        <div className="navbar">
+        <div className={`navbar ${show && "navbar_black"}`}>
             <img className="netflix-logo logo" src={logo} alt="Netflix-logo" />
             <img className="smiley logo" src={smiley} alt="smiley-logo" />
         </div>
