bd6ffa88a68b0f244db947f336ad0d3336746106
Parth Ardeshana <parthardeshana@users.noreply.github.com>
2021-03-12 10:34:10 +0530

    aa

--- src/Banner.js
+++ src/Banner.js
@@ -13,7 +13,9 @@ function Banner() {
         }
         fetchData();
     }, [])
-
+    const truncate = (str, n) => {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    };
     return (
         <header className="banner" style={{
             backgroundSize: "cover",
@@ -26,7 +28,9 @@ function Banner() {
                     <button className="banner_button">Play</button>
                     <button className="banner_button">My List</button>
                 </div>
-                <h1 className="banner_description">{movie?.overview}</h1>
+                <h1 className="banner_description">
+                    {truncate(movie?.overview, 150)}
+                </h1>
             </div>
             <div className="banner_fadebottom"></div>
         </header>
